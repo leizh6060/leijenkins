0eba9665a92d23e9ebb6c6ba36c6403f86ca3632
leizh6060 <leizh6060@yahoo.com>
2022-06-02 17:23:15 -0400

    Update MultiMethod.groovy

--- src/org/any/MultiMethod.groovy
+++ src/org/any/MultiMethod.groovy
@@ -8,7 +8,7 @@ class MultiMethod {
     def foo() {
         steps.echo "Hello foo from src/org/any/MultiMethod.groovy"
         steps.println"lei"
-        steps.getClass()
+        println (steps.getClass())
     }
     def bar() {
         steps.echo "Hello bar from src/org/any/MultiMethod.groovy"
